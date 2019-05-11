014302fcd70dddefd1bb7bed2d67a9474eb57230
jieKaMi <1196557363@qq.com>
2019-05-11 15:37:42 +0800

    Initial commit

---
+++ hello01/src/com/atguigu/bean/Comment.java
@@ -0,0 +1,25 @@
+package com.atguigu.bean;
+
+/**
+ * ClassName: Comment <br/>
+ * Description: <br/>
+ * date: 2019\5\11 0011 9:49<br/>
+ *
+ * @author Administrator<br/>
+ * @since JDK 1.8
+ */
+public class Comment {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+    }
+    public void method (){
+
+    }
+}
